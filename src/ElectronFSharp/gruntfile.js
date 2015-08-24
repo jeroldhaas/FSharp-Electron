b5eb2e9907d2784b6292b939c9d336f04933e453
Jerold Haas <jeroldhaas@gmail.com>
2015-08-24 05:20:28 -0400

    add some semblence of previous project (no mess now) :confetti_ball:

---
+++ src/ElectronFSharp/gruntfile.js
@@ -0,0 +1,22 @@
+﻿module.exports = function (grunt) {
+
+    // project configuration
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+            },
+            build: {
+                src: 'src/<%= pkg.name %>.js',
+                dest: 'build/<%= pkg.name %>.min.js'
+            }
+        }
+    });
+    
+    // load the plugin that provides the uglify task
+    grunt.loadTasks('grunt-contrib-uglify');
+    
+    // default task(s).
+    grunt.registerInitTask('default', ['uglify']);
+};
