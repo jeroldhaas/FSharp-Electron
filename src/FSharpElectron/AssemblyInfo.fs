3e7f9dfef727832196aa7f5dbd3d15ab9190912d
Jerold Haas <jeroldhaas@gmail.com>
2015-08-24 04:29:15 -0400

    initial creation

---
+++ src/FSharpElectron/AssemblyInfo.fs
@@ -0,0 +1,12 @@
+﻿namespace System
+open System.Reflection
+
+[<assembly: AssemblyTitleAttribute("FSharpElectron")>]
+[<assembly: AssemblyProductAttribute("FSharpElectron")>]
+[<assembly: AssemblyDescriptionAttribute("Binding for WebSharper to Electron")>]
+[<assembly: AssemblyVersionAttribute("1.0")>]
+[<assembly: AssemblyFileVersionAttribute("1.0")>]
+do ()
+
+module internal AssemblyVersionInformation =
+    let [<Literal>] Version = "1.0"
